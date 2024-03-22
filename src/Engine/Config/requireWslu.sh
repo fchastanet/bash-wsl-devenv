765d92c490edf778fab32b1d20f79158eb8e6577
François Chastanet <fchastanet@wiley.com>
2024-03-22 13:21:52 +0100

    Engine::Config::requireWslu fixed wsl interoperability issue due to wsl bug

--- src/Engine/Config/requireWslu.sh
+++ src/Engine/Config/requireWslu.sh
@@ -6,5 +6,16 @@ Engine::Config::requireWslu() {
     Log::displayInfo "Installing pre-requisite Wslu : wslvar, wslpath, ... commands"
     Linux::Apt::update
     Linux::Apt::install --no-install-recommends wslu
+
+    # @see https://github.com/microsoft/WSL/issues/8843#issuecomment-1792256894
+    Log::displayInfo "Fix wsl interoperability due to wsl bug"
+    if [[ 
+      ! -f /usr/lib/binfmt.d/WSLInterop.conf && 
+      ! -f /etc/systemd/system/wsl-binfmt.service &&
+      -f /run/systemd/generator.early/wsl-binfmt.service
+    ]]; then
+      sudo sh -c 'echo :WSLInterop:M::MZ::/init:PF > /usr/lib/binfmt.d/WSLInterop.conf'
+      sudo ln -s /run/systemd/generator.early/wsl-binfmt.service /etc/systemd/system/wsl-binfmt.service
+    fi
   fi
 }
