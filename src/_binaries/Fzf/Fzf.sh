bd14ac8327cef489bf1617b0fbdc5df500e21f84
François Chastanet <fchastanet@wiley.com>
2024-03-31 22:12:02 +0200

    Fzf - install .bash-dev-env/interactive.d/fzf.sh using Install::structure

--- src/_binaries/Fzf/Fzf.sh
+++ src/_binaries/Fzf/Fzf.sh
@@ -3,6 +3,7 @@
 # ROOT_DIR_RELATIVE_TO_BIN_DIR=..
 # FACADE
 # IMPLEMENT InstallScripts::interface
+# EMBED "${BASH_DEV_ENV_ROOT_DIR}/src/_binaries/Fzf/conf" as conf_dir
 
 .INCLUDE "$(dynamicTemplateDir "_includes/_installScript.tpl")"
 
@@ -51,10 +52,20 @@ install() {
   SUDO=sudo INSTALL_CALLBACK=fzfInstall Github::upgradeRelease \
     "/usr/local/bin/fzf" \
     "https://github.com/junegunn/fzf/releases/download/@latestVersion@/fzf-@latestVersion@-linux_amd64.tar.gz"
+  
+  local configDir
+  # shellcheck disable=SC2154
+  configDir="$(Conf::getOverriddenDir "${embed_dir_conf_dir}" "${CONF_OVERRIDE_DIR}/$(scriptName)")"
+  # shellcheck disable=SC2154
+  OVERWRITE_CONFIG_FILES=1 \
+  PRETTY_ROOT_DIR="$(dirname "${embed_dir_conf_dir}")" \
+  Install::structure \
+    "${configDir}/.bash-dev-env" "${USER_HOME}/.bash-dev-env"
 }
 
 testInstall() {
   Version::checkMinimal "fzf" --version "0.44.1" || return 1
+  Assert::fileExists "${USER_HOME}/.bash-dev-env/interactive.d/fzf.sh"
 }
 
 configure() { :; }
