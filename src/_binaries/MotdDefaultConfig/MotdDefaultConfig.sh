aff29fc75a3bcce34efdbcfabead65b362f7aa06
François Chastanet <fchastanet@wiley.com>
2024-04-06 15:22:26 +0200

    Motd changes
    - removed update-notifier-common apt package
    - integrated installation result in motd
    - removed unneeded motd parts

--- src/_binaries/MotdDefaultConfig/MotdDefaultConfig.sh
+++ src/_binaries/MotdDefaultConfig/MotdDefaultConfig.sh
@@ -16,7 +16,6 @@ helpDescription() {
 }
 
 dependencies() {
-  echo "Motd"
   echo "Anacron"
 }
 
@@ -56,11 +55,23 @@ configure() {
     "/etc"
 
   # disable some parts
-  sudo chmod 600 /etc/update-motd.d/00-header
-  sudo chmod 600 /etc/update-motd.d/10-help-text
+  sudo chmod 600 \
+    /etc/update-motd.d/00-header \
+    /etc/update-motd.d/10-help-text \
+    /etc/update-motd.d/50-motd-news \
+    /etc/update-motd.d/90-updates-available \
+    /etc/update-motd.d/91-contract-ua-esm-status \
+    /etc/update-motd.d/91-release-upgrade \
+    /etc/update-motd.d/92-unattended-upgrades \
+    /etc/update-motd.d/95-hwe-eol \
+    || true
+
   if [[ -f /usr/share/landscape/landscape-sysinfo.wrapper ]]; then
     sudo chmod 600 /usr/share/landscape/landscape-sysinfo.wrapper
   fi
+  sudo sed -i -E \
+    -e "s#^BASH_DEV_ENV_LOGS_DIR=.*\$#BASH_DEV_ENV_LOGS_DIR=${LOGS_DIR}#" \
+    /etc/update-motd.d/03-wsl-automatic-upgrade
 
   # update motd cache
   sudo update-motd >/dev/null
@@ -72,9 +83,13 @@ testConfigure() {
   Assert::fileExecutable "/etc/update-motd.d/00-wsl-header" || ((++failures))
   Assert::fileExecutable "/etc/update-motd.d/01-wsl-sysinfo" || ((++failures))
   Assert::fileExecutable "/etc/update-motd.d/03-wsl-automatic-upgrade" || ((++failures))
+  if grep -E -q '^BASH_DEV_ENV_LOGS_DIR=/invalid$' /etc/update-motd.d/03-wsl-automatic-upgrade; then
+    Log::displayError "/etc/update-motd.d/03-wsl-automatic-upgrade contain invalid path"
+    ((++failures))
+  fi
   Assert::fileNotExecutable "/etc/update-motd.d/10-help-text" root root || ((++failures))
   Assert::fileNotExecutable "/etc/update-motd.d/00-header" root root || ((++failures))
-
+  
   Assert::fileExecutable "/etc/cron.daily/motd" || ((++failures))
 
   Assert::fileExists "${USER_HOME}/.bash-dev-env/profile.d/motd.sh" || ((++failures))
