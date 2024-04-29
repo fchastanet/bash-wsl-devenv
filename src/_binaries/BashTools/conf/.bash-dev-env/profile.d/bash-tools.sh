3fe9d5d6ef4852b26895efdf5871525ebc44aba4
François Chastanet <fchastanet@wiley.com>
2024-04-29 11:30:41 +0200

    Various install fixes
    - Export - delete all eventual id_rsa key
    - Export - deleteFolderExcept keep .gitignore instead of .gitkeep
    - BashTools - added ~/.bash-dev-env/profile.d/bash-tools.sh

---
+++ src/_binaries/BashTools/conf/.bash-dev-env/profile.d/bash-tools.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+###############################################################################
+# DO NOT EDIT, THIS FILE CAN BE UPDATED WITHOUT NOTICE
+###############################################################################
+
+[[ :${PATH}: == *":${HOME}/fchastanet/bash-tools/bin:"* ]] ||
+  PATH="${HOME}/fchastanet/bash-tools/bin:${PATH}"
+
+export PATH
