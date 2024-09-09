244712df9be41fff85c82e501dc2d074326be533
François Chastanet <fchastanet@wiley.com>
2024-09-09 20:09:30 +0200

    Added GoGvm

---
+++ src/_installScripts/GoGvm/GoGvm.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+helpDescription() {
+  echo "gvm tool allows to switch from one go version to another."
+}
+
+dependencies() {
+  echo "installScripts/Go"
+}
+
+fortunes() {
+  echo -e "${__INFO_COLOR}$(scriptName)${__RESET_COLOR} -- use gvm to switch go version"
+  echo "%"
+}
+
+# jscpd:ignore-start
+listVariables() { :; }
+helpVariables() { :; }
+defaultVariables() { :; }
+checkVariables() { :; }
+breakOnConfigFailure() { :; }
+breakOnTestFailure() { :; }
+isInstallImplemented() { :; }
+isConfigureImplemented() { :; }
+isTestConfigureImplemented() { :; }
+isTestInstallImplemented() { :; }
+# jscpd:ignore-end
+
+install() {
+  # shellcheck source=/dev/null
+  source "${HOME}/.bash-dev-env/profile.d/golang.sh" || ((++failures))
+
+  mkdir -p "${HOME}/.local/bin"
+  curl -o "${HOME}/.local/bin/gvm" \
+    -L "https://github.com/devnw/gvm/releases/download/latest/gvm" || ((++failures))
+  chmod +x "${HOME}/.local/bin/gvm"
+
+  return "${failures}"
+}
+
+testInstall() {
+  local -i failures=0
+  # shellcheck source=/dev/null
+  Assert::commandExists gvm || ((++failures))
+
+  return "${failures}"
+}
+
+configure() {
+  # install latest version only the first time
+  if [[ -f "${HOME}/.gvm/latestVersionInitialized" ]]; then
+    Log::displaySkipped "go latest version is installed only the first time, use gvm command to switch go version"
+  else
+    Log::displayInfo "Retrieve list of go version available"
+    # install latest version
+    local latestVersion
+    latestVersion="$(curl 'https://go.dev/VERSION?m=text' |
+      grep -Eoh '[0-9]+\..*'
+    )"
+
+    Log::displayInfo "Installing go latest version is ${latestVersion}"
+    gvm "${latestVersion}" -s
+
+    touch "${HOME}/.gvm/latestVersionInitialized"
+  fi
+}
+
+testConfigure() {
+  local failures=0
+  Assert::fileExists "${HOME}/.bash-dev-env/profile.d/golang.sh" || ((++failures))
+  # shellcheck source=src/_binaries/Go/conf/.bash-dev-env/profile.d/golang.sh
+  source "${HOME}/.bash-dev-env/profile.d/golang.sh" || ((++failures))
+  Version::checkMinimal "go" "version" "1.22.1" || ((++failures))
+  return "${failures}"
+}
