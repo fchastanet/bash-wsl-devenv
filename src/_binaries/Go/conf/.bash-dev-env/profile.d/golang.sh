614dddde56ee901d183932729430d498c26e301b
François Chastanet <fchastanet@wiley.com>
2024-09-22 21:35:25 +0200

    moved GoGvm to Go folder and removed old binary

--- src/_binaries/Go/conf/.bash-dev-env/profile.d/golang.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-###############################################################################
-# DO NOT EDIT, THIS FILE CAN BE UPDATED WITHOUT NOTICE
-###############################################################################
-
-[[ :${PATH}: =~ (^|:)${HOME}/golang/go/bin(:|$) ]] || PATH=${HOME}/golang/go/bin:${PATH}
-[[ :${PATH}: =~ (^|:)${HOME}/go/bin(:|$) ]] || PATH=${HOME}/go/bin:${PATH}
