c8be79d5bc16bf96c3330e91694d75e492b6b3ae
François Chastanet <fchastanet@wiley.com>
2024-09-22 12:58:16 +0200

    converted Fasd binary using go compiler

--- src/_installScripts/BashUtils/Fasd.sh
+++ src/_installScripts/BashUtils/Fasd.sh
@@ -1,44 +1,25 @@
 #!/usr/bin/env bash
-# BIN_FILE=${BASH_DEV_ENV_ROOT_DIR}/installScripts/Fasd
-# ROOT_DIR_RELATIVE_TO_BIN_DIR=..
-# FACADE
-# IMPLEMENT InstallScripts::interface
-# EMBED "${BASH_DEV_ENV_ROOT_DIR}/src/_binaries/Fasd/conf" as conf_dir
-
-.INCLUDE "$(dynamicTemplateDir "_includes/_installScript.tpl")"
-
-scriptName() {
-  echo "Fasd"
-}
+# @embed "${BASH_DEV_ENV_ROOT_DIR}/src/_installScripts/BashUtils/Fasd-conf" as conf_dir
 
 helpDescription() {
-  if [[ "${USER_SHELL}" = "/bin/bash" || "${USER_SHELL}" = "/usr/bin/bash" ]]; then
-    echo "$(scriptName) -  configuration for bash shell only allows to search files quickly"
-    echo "Following aliases are available:"
-    echo "  - alias 'a' - search files/directories based on a pattern"
-    echo "  - alias 'd' - search directories based on a pattern"
-    echo "  - alias 'f' - search files based on a pattern"
-    echo "  - alias 'si' - search most used files/directories in interactive mode(index)"
-    echo "  - alias 'sid' - search most used directories in interactive mode(index)"
-    echo "  - alias 'sif' - search most used files in interactive mode(index)"
-    echo "  - alias 'z' - allows to switch to the directory matching the most the given pattern"
-    echo "  - alias 'zz' - allows to select interactively one of the most used directory by index"
-    echo '%'
-  fi
+  echo "$(scriptName) - configuration for bash shell only allows to search files quickly"
+  echo "Following aliases are available:"
+  echo "  - alias 'a' - search files/directories based on a pattern"
+  echo "  - alias 'd' - search directories based on a pattern"
+  echo "  - alias 'f' - search files based on a pattern"
+  echo "  - alias 'si' - search most used files/directories in interactive mode(index)"
+  echo "  - alias 'sid' - search most used directories in interactive mode(index)"
+  echo "  - alias 'sif' - search most used files in interactive mode(index)"
+  echo "  - alias 'z' - allows to switch to the directory matching the most the given pattern"
+  echo "  - alias 'zz' - allows to select interactively one of the most used directory by index"
 }
 
-# jscpd:ignore-start
-dependencies() { :; }
-helpVariables() { :; }
-listVariables() { :; }
-defaultVariables() { :; }
-checkVariables() { :; }
-breakOnConfigFailure() { :; }
-breakOnTestFailure() { :; }
-# jscpd:ignore-end
+isBash() {
+  [[ "${USER_SHELL}" = "/bin/bash" || "${USER_SHELL}" = "/usr/bin/bash" ]]
+}
 
 fortunes() {
-  if [[ "${USER_SHELL}" = "/usr/bin/bash" ]]; then
+  if isBash; then
     if command -v fasd &>/dev/null; then
       echo -e "${__INFO_COLOR}$(scriptName)${__RESET_COLOR} -- ${__HELP_EXAMPLE}z <directory>${__RESET_COLOR} to easily change directory -- see ${__HELP_EXAMPLE}<https://github.com/clvv/fasd>${__RESET_COLOR}."
       echo "%"
@@ -50,6 +31,20 @@ fortunes() {
     fi
   fi
 }
+
+# jscpd:ignore-start
+dependencies() { :; }
+listVariables() { :; }
+helpVariables() { :; }
+defaultVariables() { :; }
+checkVariables() { :; }
+breakOnConfigFailure() { :; }
+breakOnTestFailure() { :; }
+isInstallImplemented() { :; }
+isConfigureImplemented() { :; }
+isTestConfigureImplemented() { :; }
+isTestInstallImplemented() { :; }
+# jscpd:ignore-end
 
 isUbuntuMinimum24() {
   Version::compare "${VERSION_ID}" "24.04"
@@ -76,6 +71,7 @@ configure() {
     "${CONF_OVERRIDE_DIR}/$(scriptName)" \
     ".bash-dev-env"
 }
+
 testConfigure() {
   local -i failures=0
   Assert::fileExists "${HOME}/.bash-dev-env/aliases.d/fasd.bash" || ((++failures))
