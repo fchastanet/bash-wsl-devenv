7d9d0025a34889d1c7aad2293536c81636fb549b
François Chastanet <fchastanet@wiley.com>
2024-09-28 14:59:23 +0200

    converted installBashTools binary using go compiler
    - use bash-compiler v1.0.2
      - fixed argument default value

---
+++ src/_tools/installBashTools-options.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+longDescriptionFunction() {
+  echo "Installs https://github.com/fchastanet/bash-tools"
+  echo
+  echo "Collection of several bash tools:"
+  echo "  - db import"
+  echo "  - db query multiple database or apply script"
+  echo "  - git tools"
+  echo "  - docker containers easy cli"
+  echo "  - ..."
+}
