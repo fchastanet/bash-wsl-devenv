53b63fbf8b6085f42bf0603ca577e8cf59443ad8
François Chastanet <fchastanet@wiley.com>
2024-12-21 15:54:55 +0100

    Add support for plocate in Ubuntu 22.04 and added Version::isUbuntuMinimum

---
+++ src/Version/isUbuntuMinimum.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# @description Check if the current Ubuntu version
+# is greater or equal to the provided version
+# @arg $1 string Expected minimum version
+# @exitcode 0 If the current Ubuntu version is greater or equal
+# to the provided version
+Version::isUbuntuMinimum() {
+  local expectedMinimumVersion="$1"
+
+  Version::compare "${VERSION_ID}" "${expectedMinimumVersion}"
+  local -r comparisonResult=$?
+  (( comparisonResult == 0 || comparisonResult == 1 ))
+}
--- src/_installScripts/BashUtils/MLocate.sh
+++ src/_installScripts/BashUtils/MLocate.sh
@@ -23,13 +23,31 @@ isTestInstallImplemented() { :; }
 # jscpd:ignore-end
 
 install() {
+  if Version::isUbuntuMinimum "22.04"; then
+    # since 22.04 mlocate has been replaced by plocate
+    installFromUbuntu22
+  else
+    installFromUbuntu20
+  fi
+}
+
+installFromUbuntu22() {
+  if ! Linux::Apt::isPackageInstalled plocate; then
+    Log::displayInfo "install plocate required packages"
+    Linux::Apt::update
+    Linux::Apt::install --no-install-recommends \
+      -o APT::Update::Exclude=updatedb plocate
+  fi
+}
+
+installFromUbuntu20() {
   # download mlocate package
   if ! dpkg -s mlocate &>/dev/null; then
     (
       mkdir -p /tmp/mlocate true
       cd /tmp/mlocate || exit 1
       # download mlocate package
-      apt-get download mlocate
+      sudo apt-get download mlocate
       # extract it
       dpkg-deb -R mlocate_*_amd64.deb /tmp/mlocate/deb
       rm ./*.deb
@@ -37,41 +55,80 @@ install() {
       BASE_MNT_C="$(mount | grep 'path=C:\\' | awk -F ' ' '{print $3}')"
       # update configuration in order to remove /mnt/c, docker and other cache directories
       PRUNEPATHS="${BASE_MNT_C} /var/cache /var/www /var/lib/docker ${USERHOME}/.npm ${USERHOME}/.cache"
-      sed -i -r \
+      sudo sed -i -r \
         "s#^PRUNEPATHS=\"(.*)\"\$#PRUNEPATHS=\"\1 ${PRUNEPATHS} \"#" \
         deb/etc/updatedb.conf
-      sed -i -r 's/^# PRUNENAMES=/PRUNENAMES=/' deb/etc/updatedb.conf
+      sudo sed -i -r 's/^# PRUNENAMES=/PRUNENAMES=/' deb/etc/updatedb.conf
       # recompress package
-      dpkg-deb -b deb mlocate.deb
+      sudo dpkg-deb -b deb mlocate.deb
       rm -Rf deb
       # install the new package
-      dpkg -i mlocate.deb
+      sudo dpkg -i mlocate.deb
     )
   fi
 }
 
 testInstall() {
+  if Version::isUbuntuMinimum "22.04"; then
+    # since 22.04 mlocate has been replaced by plocate
+    testInstallFromUbuntu22
+  else
+    testInstallFromUbuntu20
+  fi
+}
+
+testInstallFromUbuntu22() {
   local -i failures=0
-  dpkg -s mlocate &>/dev/null || {
-    Log::displayError "missing mlocate binary"
+  dpkg -s plocate &>/dev/null || {
+    Log::displayError "missing plocate package"
     ((++failures))
   }
   Assert::commandExists "locate" || ((++failures))
   return "${failures}"
 }
 
-configure() {
-  # update database
-  export MYPRUNEPATHS="/tmp /var/spool /media /var/lib/os-prober /var/lib/ceph /home/.ecryptfs /var/lib/schroot /var/cache /var/www /var/lib/docker"
-  dpkg --configure -a
+testInstallFromUbuntu20() {
+  local -i failures=0
+  dpkg -s mlocate &>/dev/null || {
+    Log::displayError "missing mlocate package"
+    ((++failures))
+  }
+  Assert::commandExists "locate" || ((++failures))
+  return "${failures}"
+}
 
+configureMlocate() {
+  local -r BASE_MNT_C="$(mount | grep 'path=C:\\' | awk -F ' ' '{print $3}')"
+  local -r prunePaths=(
+    "${BASE_MNT_C}"
+    "/var/cache"
+    "/var/www"
+    "/var/lib/docker"
+    "${USERHOME}/.npm"
+    "${USERHOME}/.cache"
+  )
+  # shellcheck disable=SC1003
+  sudo sed -i -E \
+    -e "s#^PRUNEPATHS=\"(.*)\"\$#PRUNEPATHS=\"\1 ${prunePaths[*]} \"#" \
+    -e 's/^# PRUNENAMES="/PRUNENAMES="node_modules /' \
+    -e '$a\' \
+    -e "# updated by bash-dev-env" \
+    /etc/updatedb.conf
+}
+
+configure() {
+  if ! grep -q '# updated by bash-dev-env' /etc/updatedb.conf; then
+    configureMlocate
+  fi
   Log::displayInfo "Update completion database, can take a while ..."
-  time updatedb
+  time sudo updatedb
 }
 
 testConfigure() {
   local -i failures=0
-  grep -E '^PRUNEPATHS=".*( /mnt/c){1}.*"$' /etc/updatedb.conf || ((++failures))
-  grep -E '^PRUNEPATHS=".*( /c){1}.*"$' /etc/updatedb.conf || ((++failures))
+  grep -q -E '^PRUNEPATHS=".*( /mnt/c){1}.*"$' /etc/updatedb.conf || {
+    Log::displayError "missing /mnt/c in /etc/updatedb.conf PRUNEPATHS"
+    ((++failures))
+  }
   return "${failures}"
 }
